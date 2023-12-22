2bf6cb5ac1906e41e97c52ec066492fb59467390
LeDiamantRouge <dj.poutchcres@gmail.com>
2023-12-22 13:10:41 +0100

    first commit

---
+++ src/main/java/fr/lediamantrouge/customplaceholder/CustomPlaceholder.java
@@ -0,0 +1,63 @@
+package fr.lediamantrouge.customplaceholder;
+
+import fr.lediamantrouge.customplaceholder.command.CustomPlaceholderCommand;
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public final class CustomPlaceholder extends JavaPlugin {
+
+    private static CustomPlaceholder instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+
+        registerPlaceholder();
+
+        getCommand("customplaceholder").setExecutor(new CustomPlaceholderCommand());
+    }
+
+    public void registerPlaceholder() {
+        if (getConfig().getConfigurationSection("custom-placeholder") != null) {
+            for (String key : getConfig().getConfigurationSection("custom-placeholder").getKeys(false)) {
+                if (!PlaceholderAPI.isRegistered(key)) {
+                    new PlaceholderExpansion() {
+
+                        @Override
+                        public @NotNull String getIdentifier() {
+                            return key;
+                        }
+
+                        @Override
+                        public @NotNull String getAuthor() {
+                            return "CustomPlaceholder";
+                        }
+
+                        @Override
+                        public @NotNull String getVersion() {
+                            return CustomPlaceholder.this.getDescription().getVersion();
+                        }
+
+                        @Override
+                        public @Nullable String onPlaceholderRequest(Player player, @NotNull String params) {
+                            return getConfig().getConfigurationSection("custom-placeholder").getString(key);
+                        }
+                    }.register();
+                    getLogger().info("Registered custom placeholder " + key);
+                }
+            }
+            getLogger().info("All custom placeholders have been registered");
+        } else {
+            getLogger().info("No placustom placeholders have been registered !");
+        }
+    }
+
+    public static CustomPlaceholder getInstance() {
+        return instance;
+    }
+}
---
+++ src/main/java/fr/lediamantrouge/customplaceholder/command/CustomPlaceholderCommand.java
@@ -0,0 +1,20 @@
+package fr.lediamantrouge.customplaceholder.command;
+
+import fr.lediamantrouge.customplaceholder.CustomPlaceholder;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CustomPlaceholderCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
+        if (args.length == 0 || !args[0].equalsIgnoreCase("reload")) {
+            s.sendMessage("§cUsage: /" + label + " <reload>");
+        }
+        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+            CustomPlaceholder.getInstance().reloadConfig();
+            s.sendMessage("§aConfiguration reloaded !");
+        }
+        return true;
+    }
+}
